d577e9c692d49a6bc35abf877b439eb3a01bafaa
Erunam <dubaj.daniel@gmail.com>
2026-04-24 23:20:12 +0100

    Problem with *ngFor again. Fixed can go ahead again.

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -2,26 +2,31 @@ import { Component } from '@angular/core';
 import { IonHeader, IonToolbar, IonTitle, IonContent, IonButton, IonGrid, IonRow, IonCol, IonCard, IonCardHeader, IonCardTitle, IonCardSubtitle, IonCardContent } from '@ionic/angular/standalone';
 import { MovieDB } from '../services/movie-db';
 import { MyData } from '../services/data';
+import { HttpOptions } from '@capacitor/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonButton, IonGrid, IonRow, IonCol, IonCard, IonCardHeader, IonCardTitle, IonCardSubtitle, IonCardContent],
+  imports: [CommonModule, IonHeader, IonToolbar, IonTitle, IonContent, IonButton, IonGrid, IonRow, IonCol, IonCard, IonCardHeader, IonCardTitle, IonCardSubtitle, IonCardContent],
 })
 export class HomePage {
   name = "G00426106";
   keyword = "";
-  movieData: any;
+  movieData!: any;
   baseUrl = "";
   posterSize = "";
   posterBaseUrl = "";
   posterSizeIndex = 0;
+  api = "e132512c4c29d4cde63e3ee5621ba016";
+  options: HttpOptions = {
+    url : "https://api.themoviedb.org/3/trending/movie/day?api_key="
+  }
 
   constructor(private mydata: MyData, private movie: MovieDB) {}
   
   ngOnInit(){
-    this.movieData = ["", ""];
     this.runApp();
   }
 
@@ -36,6 +41,14 @@ export class HomePage {
     await this.getKeyword();
     await this.setPosterBaseUrl();
     await this.getMovies();
+
+    /*
+    this.keyword = await this.mydata.get("keyword");
+    this.options.url += this.api;
+    let result = await this.movie.get(this.options);
+    this.movieData = result.results;
+    console.log(this.movieData);
+    */
   }
 
   async searchClick(keyword: string)
@@ -78,7 +91,7 @@ export class HomePage {
     console.log("searching movie ... ");
     let result = await this.movie.getMovies( keyword );
     console.log(result);
-    this.movieData = result;
+    this.movieData = result.results;
     console.log(this.movieData);
   }
 
